339975ee42e36889883516e4b59e9ed14b441610
Hidenori Miyoshi <miyohide@gmail.com>
2025-11-03 16:33:57 +0900

    re-write

--- src/main/java/com/github/miyohide/demo/DemoApplication.java
+++ src/main/java/com/github/miyohide/demo/DemoApplication.java
@@ -1,6 +1,12 @@
 package com.github.miyohide.demo;
 
+import io.modelcontextprotocol.client.McpClient;
 import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.client.transport.ServerParameters;
+import io.modelcontextprotocol.client.transport.StdioClientTransport;
+import io.modelcontextprotocol.json.McpJsonMapper;
+
+import java.time.Duration;
 import java.util.List;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -28,12 +34,12 @@ public class DemoApplication {
     return args -> {
       ChatClient chatClient =
           chatClientBuilder
-              .defaultSystem("You are an AWS CDK expert")
+              .defaultSystem("You are an AWS expert")
               .defaultToolCallbacks(new SyncMcpToolCallbackProvider(mcpSyncClients))
-              .defaultAdvisors(
-                  MessageChatMemoryAdvisor.builder(MessageWindowChatMemory.builder().build())
-                      .build(),
-                  new SimpleLoggerAdvisor())
+            //   .defaultAdvisors(
+            //       MessageChatMemoryAdvisor.builder(MessageWindowChatMemory.builder().build())
+            //           .build(),
+            //       new SimpleLoggerAdvisor())
               .build();
 
       System.out.println("\nI'm your AI assistant.\n");
@@ -43,5 +49,24 @@ public class DemoApplication {
 
       context.close();
     };
+  }
+
+  @Bean(destroyMethod = "close")
+  public McpSyncClient mcpClient() {
+	var stdioParams = ServerParameters.builder("uvx")
+	.args("awslabs.aws-documentation-mcp-server@latest")
+	.addEnvVar("FASTMCP_LOG_LEVEL", "ERROR")
+	.addEnvVar("AWS_DOCUMENTATION_PARTITION", "aws")
+	.addEnvVar("MCP_USER_AGENT", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+	.build();
+
+	var mcpClient = McpClient.sync(new StdioClientTransport(stdioParams, McpJsonMapper.createDefault()))
+	.requestTimeout(Duration.ofSeconds(10)).build();
+
+	var init = mcpClient.initialize();
+
+	System.out.println("MCP Initialized: " + init);
+
+	return mcpClient;
   }
 }
